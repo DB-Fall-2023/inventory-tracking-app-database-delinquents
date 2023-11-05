f25755a36c0d6ecd928502a518773536678906ab
Jan Badillo <60725158+janbadillo@users.noreply.github.com>
2023-11-05 17:34:19 -0400

    commented most of the app.py to make it simpler (for testing)

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+# from flask import Flask, jsonify, request
+# from flask_cors import CORS, cross_origin
+
+# app = Flask(__name__)
+
+# #apply CORS
+# CORS(app)
+
+# @app.route('/')
+# def greeting():
+#     return 'Hello, this is the parts DB app'
+
+# if __name__ == '__main__':
+#     app.run()
+
+from flask import Flask
 
 app = Flask(__name__)
 
-#apply CORS
-CORS(app)
+@app.route("/")
+def hello_world():
+    return "<h1 style='color:green'>Hello World!</h1>"
 
-@app.route('/')
-def greeting():
-    return 'Hello, this is the parts DB app'
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
