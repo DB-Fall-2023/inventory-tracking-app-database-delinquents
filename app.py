aef1a6e2b8920bb39b2f94c5815f4e7cb8a880b0
Jan Badillo <60725158+janbadillo@users.noreply.github.com>
2023-11-05 17:43:49 -0400

    Changed to old app.py again

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-# from flask import Flask, jsonify, request
-# from flask_cors import CORS, cross_origin
-
-# app = Flask(__name__)
-
-# #apply CORS
-# CORS(app)
-
-# @app.route('/')
-# def greeting():
-#     return 'Hello, this is the parts DB app'
-
-# if __name__ == '__main__':
-#     app.run()
-
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1 style='color:green'>Hello World!</h1>"
+#apply CORS
+CORS(app)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/')
+def greeting():
+    return 'Hello, this is the parts DB app'
+
+if __name__ == '__main__':
+    app.run()
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<h1 style='color:green'>Hello World!</h1>"
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0')
